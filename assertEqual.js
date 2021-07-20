971790d8485495e384700307b4ddbd8c8471aab5
marcschul <marcschulm@gmail.com>
2021-07-20 23:22:30 +0000

    completed assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😀😀😀 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('💣💣💣 Assertation Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); // expected: fail
+assertEqual('Hello', 'Hello'); // expected: pass
+assertEqual(1, 3); // expected: fail
+assertEqual(1, 1); // expected: pass
