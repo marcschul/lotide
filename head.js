09194a3a1141e8d55baa25a5a2039e3eddc73c95
marcschul <marcschulm@gmail.com>
2021-07-20 23:50:12 +0000

    completed head.js file, with head function

---
+++ head.js
@@ -0,0 +1,19 @@
+const head = function(array) {
+  return array[0];
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {  
+    console.log(`💣💣💣 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5); // expected: pass;
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // expected: pass;
+assertEqual(head([1]), 1); // expected: pass;
+assertEqual(head([]), ); // expected: pass;
+assertEqual(head([5, 6, 7]), 6); // expected: fail;
+assertEqual(head(['lose']), 'win'); // expected: fail;
+assertEqual(head([]), 6); // expected: fail;
