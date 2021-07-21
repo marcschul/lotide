12d712354c52aba0916306b04d0ce7f493b7b22b
marcschul <marcschulm@gmail.com>
2021-07-21 00:20:40 +0000

    added tail.js, shallow copy of array with only array's tail

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ const head = function(array) {
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
-  } else {  
+  } else {
     console.log(`💣💣💣 Assertation Failed: ${actual} !== ${expected}`);
   }
 };
@@ -13,7 +13,7 @@ const assertEqual = function(actual, expected) {
 assertEqual(head([5, 6, 7]), 5); // expected: pass;
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // expected: pass;
 assertEqual(head([1]), 1); // expected: pass;
-assertEqual(head([]), ); // expected: pass;
+assertEqual(head([]),); // expected: pass;
 assertEqual(head([5, 6, 7]), 6); // expected: fail;
 assertEqual(head(['lose']), 'win'); // expected: fail;
 assertEqual(head([]), 6); // expected: fail;
