bbe314ff715e97e748d37a4b7086c2be6e27f4fc
marcschul <marcschulm@gmail.com>
2021-07-22 00:03:35 +0000

    created eqArrays, arrays equal function

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {  
+    console.log(`💣💣💣 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
