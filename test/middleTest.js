f08050b6900f7221c25c582c5d06433a6e18f618
marcschul <marcschulm@gmail.com>
2021-08-08 21:19:57 +0000

    m&c refractor

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,11 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+const middle  = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [5] for [5]", () => {
+    assert.deepEqual(middle([5]), []); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,11 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-
-assertEqual(JSON.stringify(tail([1, 2, 3, 4])), JSON.stringify([2, 3, 4])); // => pass
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(JSON.stringify(tail([1])), JSON.stringify([])); // => pass;
-assertEqual(JSON.stringify(tail([])), JSON.stringify([])); // => pass;
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
