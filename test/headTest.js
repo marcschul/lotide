ccca08f787b18ecb1292f2f33915b7624cbf90d7
marcschul <marcschulm@gmail.com>
2021-08-08 20:37:47 +0000

    init

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,11 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-
-// TEST CASE(S)
-assertEqual(head([5, 6, 7]), 5); // expected: pass;
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // expected: pass;
-assertEqual(head([1]), 1); // expected: pass;
-assertEqual(head([]),); // expected: pass;
-assertEqual(head([5, 6, 7]), 6); // expected: fail;
-assertEqual(head(['lose']), 'win'); // expected: fail;
-assertEqual(head([]), 6); // expected: fail;
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
