83149ebebfd44db37327ecf2d44324f41e3369c6
marcschul <marcschulm@gmail.com>
2021-09-04 21:11:26 +0000

    fix bug producing incorrect results. Refactor code with short circuit if statements

--- middle.js
+++ middle.js
@@ -2,16 +2,22 @@ const _ = require('./index');
 
 const middle = function(array) {
   const result = [];
-  if (array.length === 1 || array.length === 2) {
-    return result;
-  } else if (array.length % 2 === 1) {
-    result.push(Math.ceil(array.length / 2));
-    return result;
-  } else if (array.length % 2 === 0) {
-    result.push(array.length / 2);
-    result.push((array.length / 2) + 1);
+  const length = array.length;
+  
+  if (length === 1 || length === 2) {
     return result;
   }
+
+  if (length % 2 === 1) {
+    result.push(array[Math.floor(length / 2)]);
+    return result;
+  }
+
+  if (length % 2 === 0) {
+    result.push(array[(length / 2) - 1], array[(length / 2)]);
+    return result;
+  }
+  return null;
 };
 
 module.exports = middle;
