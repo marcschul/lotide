bee9869cb4ee37c3dd169c7a042339850d81fb27
marcschul <marcschulm@gmail.com>
2021-09-04 21:12:34 +0000

    fix bug producing incorrect results. Refactor code using .filter() and .includes() methods

--- without.js
+++ without.js
@@ -1,20 +1,8 @@
 const _ = require('./index');
 
 const without = function(source, itemsToRemove) {
-  // shallow copy of source array
-  let newArr = [];
-  for (let element of source) {
-    newArr.push(element);
-  }
-  // removes elements from newArr found in itemsToRemove
-  for (let i = 0; i < itemsToRemove.length; i++) {
-    for (let j = 0; j < newArr.length; j++) {
-      if (itemsToRemove[i] === newArr[j]) {
-        newArr.splice(j, 1);
-      }
-    }
-  }
-  return newArr;
-};
+  const result = source.filter(item => !itemsToRemove.includes(item));
+  return result;
+}
 
 module.exports = without;
